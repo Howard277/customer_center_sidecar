c08b2936d2c8dbd566f3cfe765f8671448acf429
wuketao <wuketao@houbank.com>
2018-11-29 13:36:16 +0800

    删除多余注释

--- src/main/java/com/test/customercentersidecar/Application.java
+++ src/main/java/com/test/customercentersidecar/Application.java
@@ -2,11 +2,9 @@ package com.test.customercentersidecar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 
 @EnableSidecar
-//@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
